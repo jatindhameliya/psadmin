98994a5dab5667eba890c5498b74165216201ffe
Jatin Dhameliya <jatin.d@premware.in>
2021-08-18 17:03:37 +0530

    folder app structure

---
+++ routes/countries.js
@@ -0,0 +1,39 @@
+const dotenv = require('dotenv').config();
+const routes = require('express').Router();
+const ResponseManager = require('../utilities/response.manager');
+const async = require('async');
+const appURI = process.env.DOMAIN_NAME;
+routes.get('/', (req, res, next) => {
+	if (req.session.admin_id != '' && req.session.admin_id != undefined) {
+		res.render('countries/list', { 'ociurl': process.env.S3_IMAGE_URL });
+	} else {
+		var goto = appURI;
+		res.writeHead(302, { 'Location': goto });
+		res.end();
+	}
+});
+routes.get('/add', (req, res, next) => {
+	if (req.session.admin_id != '' && req.session.admin_id != undefined) {
+		res.render('countries/country', { 'ociurl': process.env.S3_IMAGE_URL });
+	} else {
+		var goto = appURI;
+		res.writeHead(302, { 'Location': goto });
+		res.end();
+	}
+});
+routes.get('/edit', (req, res, next) => {
+	if (req.session.admin_id != '' && req.session.admin_id != undefined) {
+		if (req.query.cid != '' && req.query.cid != undefined && req.query.cid != 0 && req.query.cid != null) {
+			res.render('countries/category', { 'ociurl': process.env.S3_IMAGE_URL });
+		} else {
+			var goto = process.env.APP_URI + '/countries';
+			res.redirect(goto);
+			res.end();
+		}
+	} else {
+		var goto = appURI;
+		res.redirect(goto);
+		res.end();
+	}
+});
+module.exports = routes;
---
+++ routes/orders.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv').config();
+const routes = require('express').Router();
+const ResponseManager = require('../utilities/response.manager');
+const async = require('async');
+const appURI = process.env.DOMAIN_NAME;
+routes.get('/', (req, res, next) => {
+	if (req.session.admin_id != '' && req.session.admin_id != undefined) {
+		res.render('orders/list', { 'ociurl': process.env.S3_IMAGE_URL });
+	} else {
+		var goto = appURI;
+		res.writeHead(302, { 'Location': goto });
+		res.end();
+	}
+});
+module.exports = routes;
